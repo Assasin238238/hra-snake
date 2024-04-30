c3a5f96658b0debcb9d0bba6749a9f90e0646b16
Assasin238238 <harnach2006@gmail.com>
2024-04-30 14:25:58 +0200

    bnmg

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snake hp</title>
+    <title>Snake HP</title>
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    <?php include "php/header.php" ?>
-    <a class="btn" href="php/snake.php">Play</a>
+    <div class="header">
+        <h1>Snake Game</h1>
+    </div>
+    <div class="section">
+            <h2>About Snake HP</h2>
+            <p>This is a simple snake game website.</p>
+            <h2>Play Snake</h2>
+            <a href="php/snake.php" class="play-btn">Play</a>
+    </div>
+    
+            
+    
+    
+    <footer>
+        &copy; 2024; Assasin238. Všechna práva vyhrazena.
+    </footer>
 </body>
 </html>
