2d1a9f12960c71df444b13a1b1783c4c3e1d4c3f
Assasin238238 <harnach2006@gmail.com>
2024-05-03 22:29:48 +0200

    added icons 0.46

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- Skript pro zobrazení 3D modelu -->
     <script type="module" src="https://unpkg.com/@splinetool/viewer@1.2.5/build/spline-viewer.js"></script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+
     <style>
         /* Styly pro kontejner s 3D modelem */
         .model-container {
