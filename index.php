135f0cd68fa64df59d5602cf7058e05b194ed871
Assasin238238 <harnach2006@gmail.com>
2024-05-05 15:44:50 +0200

    added comment 0.51

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 
             
         }
-
+        /* Leaving website :( */
         let docTitle = document.title;
         window.addEventListener("blur", ()=>{
             document.title = "Come back! :(";
