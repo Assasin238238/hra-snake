972a51e52cfd9d9bb48187b03d7adf545271c9b4
Assasin238238 <harnach2006@gmail.com>
2024-05-08 23:44:47 +0200

    changed title - about.php; added html:5 - contact-html; version: 0.60

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-    <title>Snake Game</title>
+    <title>About - Snake Game</title>
 </head>
 <body>
     <header>
