11b3d850aac026837ddf86b57ef790f4883b961e
Assasin238238 <harnach2006@gmail.com>
2024-05-17 22:38:12 +0200

    version: 1.02

--- scripts/script.js
+++ scripts/script.js
@@ -94,7 +94,7 @@ function move() {
   snake.unshift(head);
 
   // Check if the player has reached length 50
-  if (snake.length - 1 === 2) {
+  if (snake.length - 1 === 50) {
     // Play the music
     const backgroundMusic = document.getElementById('background-music');
     backgroundMusic.volume = 0.5; // Set volume to 50%
